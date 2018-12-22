b8bdffccfe7a3f1c7f7bd52f66aa67a54c71977e
冯文议 <xfsy_2015@163.com>
2018-12-22 22:09:50 +0800

    Initial commit

---
+++ feign-api-service/src/main/java/com/fengwenyi/feignapiservice/FeignApiServiceApplication.java
@@ -0,0 +1,14 @@
+package com.fengwenyi.feignapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignApiServiceApplication.class, args);
+    }
+
+}
+
